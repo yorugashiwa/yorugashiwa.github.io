61b9773db1f37dc0e1c5966b95e3b870d1b818c4
yorugashiwa <75733166+yorugashiwa@users.noreply.github.com>
2022-07-08 16:37:00 +0900

    Add files via upload

--- kahiwa/assets/js/main.js
+++ kahiwa/assets/js/main.js
@@ -1,1 +1,13 @@
 
+
+var moreButton = document.getElementById("more-button");
+var closeButton = document.getElementById("close-button");
+var more = document.getElementById("more");
+
+moreButton.onclick = function () {
+    more.classList.remove("hid");
+}
+
+closeButton.onclick = function () {
+    more.classList.add("hid");
+}
