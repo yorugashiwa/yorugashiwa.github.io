ab41fb82609e8013275d70cc4d20f1d776c86ee5
yorugashiwa <75733166+yorugashiwa@users.noreply.github.com>
2022-07-11 00:07:14 +0900

    Add files via upload

--- kahiwa/assets/js/main.js
+++ kahiwa/assets/js/main.js
@@ -1,3 +1,16 @@
+var menu = document.getElementById("menu");
+var hum = document.getElementById("hum");
+
+hum.onclick = function (){
+    menu.classList.toggle("visi");
+
+}
+$('#menu a[href]').on('click', function(event) {
+    $('#hum').trigger('click');
+});
+$('#menu ').on('click', function(event) {
+    $('#hum').trigger('click');
+});
 
 	$(".inline-0").modaal({
         fullscreen:true
@@ -16,4 +29,14 @@
     });
     $(".inline-5").modaal({
         fullscreen:true
+    });$('.image-1').modaal({
+        type: 'image'
+    });$('.image-2').modaal({
+        type: 'image'
+    });
+      $('.move a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+        var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+        var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+        $('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+        return false;
     });
